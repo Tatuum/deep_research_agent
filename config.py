25471c4e0e2b9e81c5d66decde686358644c2383
Tatuum <tatiana@panteleeva.it>
2025-06-17 07:19:56 +0200

    added writer_agent.py

--- config.py
+++ config.py
@@ -11,3 +11,12 @@ INSTRUCTIONS_SEARCH = "You are a research assistant. Given a search term, you se
     This will be consumed by someone syntesizing a report, so it's vital \
     to capture the essence and ignore any fluff.\
     Do not include any additional comments other than the summary itself."
+
+INSTRUCTIONS_WRITER = (
+    "You are a senior researcher tasked with writing a cohesive report for a research query. "
+    "You will be provided with the original query, and some initial research done by a research assistant.\n"
+    "You should first come up with an outline for the report that describes the structure and "
+    "flow of the report. Then, generate the report and return that as your final output.\n"
+    "The final output should be in markdown format, and it should be lengthy and detailed. Aim "
+    "for 5-10 pages of content, at least 1000 words."
+)
---
+++ writer_agent.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+from agents import Agent
+from config import INSTRUCTIONS_WRITER
+
+
+
+class ReportData(BaseModel):
+    short_summary: str
+    """A short 2-3 sentence summary of the findings."""
+
+    markdown_report: str
+    """The final report"""
+
+    follow_up_questions: list[str]
+    """Suggested topics to research further"""
+
+
+writer_agent = Agent(
+    name="WriterAgent",
+    instructions=INSTRUCTIONS_WRITER,
+    model="gpt-4o-mini",
+    output_type=ReportData,
+)
