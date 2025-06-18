53c72152b5df84d6779c381fc363287186e31940
Tatuum <tatiana@panteleeva.it>
2025-06-18 07:36:32 +0200

    added research_manager.py

---
+++ research_manager.py
@@ -0,0 +1,5 @@
+from agents import Runner, gen_trace_id
+from search_agent import search_agent
+from planner import planner_agent
+from writer_agent import writer_agent
+from email_agent import email_agent
