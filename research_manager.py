49b720bab7f07caea15227087bdb9f28f1e1b462
Tatuum <tatiana@panteleeva.it>
2025-06-20 08:49:41 +0200

    added gradio as dependency

--- research_manager.py
+++ research_manager.py
@@ -2,7 +2,7 @@ from agents import Runner, gen_trace_id,trace
 import asyncio
 from search_agent import search_agent
 from planner_agent import planner_agent, WebSearchList, WebSearchItem
-from writer_agent import writer_agent
+from writer_agent import writer_agent, ReportData
 from email_agent import email_agent
 #from typing import Dict
 
